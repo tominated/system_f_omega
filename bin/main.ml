8f00a9b1cd8c7d1cc70177b81e409d2b5fda5811
Tom Brunoli <tominated@users.noreply.github.com>
2021-07-04 14:59:18 -0400

    Added simple CEK machine that seems to work!

--- bin/main.ml
+++ bin/main.ml
@@ -4,22 +4,15 @@ open Lib
 
 let example_direct : CEK.Direct.t =
   let open CEK.Direct in
-  Plus (Plus (Int 2, Int 2), Let ("x", Int 1, Apply (Var "f", Var "x")))
-
-let example_anf : CEK.ANF.t =
-  let open CEK.ANF in
-  LetPlus
-    ( "plus1"
-    , (Int 2, Int 2)
-    , LetValue
-        ( "x"
-        , Int 1
-        , LetApply
-            ("app2", (Var "f", Var "x"), TailPlus (Var "plus0", Var "app2")) )
-    )
+  Let
+    ( "add2"
+    , Lambda ("x", Plus (Var "x", Int 2))
+    , Plus (Int 5, Apply (Var "add2", Int 3)) )
 
 let () =
   Stdio.printf "Direct:\n%s\n\n" (CEK.Direct.to_string example_direct) ;
-  Stdio.printf "ANF Manual:\n%s\n\n" (CEK.ANF.to_string example_anf) ;
-  Stdio.printf "ANF Transform:\n%s\n\n"
-    (CEK.ANF.to_string (CEK.direct_to_anf example_direct))
+  let anf = CEK.direct_to_anf example_direct in
+  Stdio.printf "ANF Transform:\n%s\n\n" (CEK.ANF.to_string anf) ;
+  Stdio.print_endline "evaluating..." ;
+  let result = CEK.CEK.evaluate anf in
+  Stdio.printf "Result: %s\n" (CEK.CEK.value_to_string result)
--- lib/CEK.ml
+++ lib/CEK.ml
@@ -65,26 +65,113 @@ module ANF = struct
           (to_string if_true) (to_string if_false)
     | TailPlus (a, b) ->
         Printf.sprintf "%s + %s" (value_to_string a) (value_to_string b)
-    | LetApply (v, (fn, arg), body) ->
-        Printf.sprintf "let %s = %s %s in\n%s" v (value_to_string fn)
+    | LetApply (x, (fn, arg), body) ->
+        Printf.sprintf "let %s = %s %s in\n%s" x (value_to_string fn)
           (value_to_string arg) (to_string body)
-    | LetPlus (v, (a, b), body) ->
-        Printf.sprintf "let %s = %s + %s in\n%s" v (value_to_string a)
+    | LetPlus (x, (a, b), body) ->
+        Printf.sprintf "let %s = %s + %s in\n%s" x (value_to_string a)
           (value_to_string b) (to_string body)
-    | LetValue (v, value, body) ->
-        Printf.sprintf "let %s = %s in\n%s" v (value_to_string value)
+    | LetValue (x, value, body) ->
+        Printf.sprintf "let %s = %s in\n%s" x (value_to_string value)
           (to_string body)
     | Value v ->
         value_to_string v
 end
 
+module CEK = struct
+  type t = ANF.t * env * kont
+
+  and env = value Map.M(String).t
+
+  and value = Bool of bool | Int of int | Closure of string * ANF.t * env
+
+  and kont = Halt | Arg of string * ANF.t * env * kont
+
+  let to_value (env : env) : ANF.value -> value = function
+    | Var v ->
+        Map.find_exn env v
+    | Int i ->
+        Int i
+    | Bool b ->
+        Bool b
+    | Lambda (arg, body) ->
+        Closure (arg, body, env)
+
+  let value_to_string : value -> string = function
+    | Int i ->
+        Int.to_string i
+    | Bool b ->
+        Bool.to_string b
+    | Closure _ ->
+        "<closure>"
+
+  let step : t -> t = function
+    | Value v, env, Arg (x, next_code, env', k) ->
+        let env'' = Map.set env' ~key:x ~data:(to_value env v) in
+        (next_code, env'', k)
+    | LetValue (x, v, body), env, k ->
+        let env' = Map.set env ~key:x ~data:(to_value env v) in
+        (body, env', k)
+    | TailIf (cond, if_true, if_false), env, k -> (
+      match to_value env cond with
+      | Bool true ->
+          (if_true, env, k)
+      | Bool false ->
+          (if_false, env, k)
+      | _ ->
+          failwith "passed a non-boolean value to if" )
+    | TailApply (fn, arg), env, k -> (
+      match to_value env fn with
+      | Closure (arg_var, closure_body, env') ->
+          let env'' = Map.set env' ~key:arg_var ~data:(to_value env arg) in
+          (closure_body, env'', k)
+      | _ ->
+          failwith "applied a non-closure value" )
+    | LetApply (x, (fn, arg), body), env, k -> (
+      match to_value env fn with
+      | Closure (arg_var, closure_body, env') ->
+          let env'' = Map.set env' ~key:arg_var ~data:(to_value env arg) in
+          (closure_body, env'', Arg (x, body, env, k))
+      | _ ->
+          failwith "applied a non-closure value" )
+    | TailPlus (a, b), env, Arg (x, next_code, env', k) -> (
+      match (to_value env a, to_value env b) with
+      | Int a, Int b ->
+          let env'' = Map.set env' ~key:x ~data:(Int (a + b)) in
+          (next_code, env'', k)
+      | _ ->
+          failwith "added a non-int value" )
+    | LetPlus (x, (a, b), body), env, k -> (
+      match (to_value env a, to_value env b) with
+      | Int a, Int b ->
+          let env' = Map.set env ~key:x ~data:(Int (a + b)) in
+          (body, env', k)
+      | _ ->
+          failwith "added a non-int value" )
+    | TailPlus _, _, _ | Value _, _, _ ->
+        failwith "invalid state"
+
+  let inject term : t =
+    let env = Map.empty (module String) in
+    let k = Arg ("__halt__", Value (Var "__halt__"), env, Halt) in
+    (term, env, k)
+
+  let evaluate term =
+    let rec go : t -> value = function
+      | Value v, env, Halt ->
+          to_value env v
+      | next ->
+          go (step next)
+    in
+    go (inject term)
+end
+
 let direct_to_anf expr =
   let fresh =
-    let last_num = ref 1 in
+    let last_num = ref 0 in
     fun prefix ->
-      let x = !last_num in
-      last_num := x + 1 ;
-      Printf.sprintf "%s%d" prefix x
+      last_num := !last_num + 1 ;
+      Printf.sprintf "%s%d" prefix !last_num
   in
   let rec trans0 : Direct.t -> ANF.t = function
     | Var x ->
